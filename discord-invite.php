d8f2bd271a33afa42443bb788fb150fb147123f9
ErsohnStyneEntertainment <ersohn.styne@gmail.com>
2023-02-01 16:50:15 +0100

    Resynced with original web

---
+++ discord-invite.php
@@ -0,0 +1,26 @@
+<?php
+$title = "Neverball - Discord";
+include("includes/header.php");
+?>
+</head>
+<body>
+	<div id="wrapper">
+	<div id="banner"><h1>Neverball</h1></div>
+
+<?php include("includes/nav.php"); ?>
+
+    <div id="main">
+        <div id="contents">
+            <h2>Discord invitation</h2>
+            <p>Choose between classic and clash game</p>
+            <dl class="add-colons">
+                <dt><a href="https://discord.gg/qnJR263Hm2">Pennyball + Neverball (Recommended)</a></dt>
+                    <dd><p>Challenge yourself with a brand-new way to play Neverball. Each player has In-game-account that you can use coins and gems you earn to play. Manage them wisely to win the game.</p>
+                        <p>See <a href="https://microsoft.com/en-us/servicesagreement">Microsoft Services Agreement</a> ("MSA").<br><i>P.S.: We take crypto! You'll see the "#verify" channel before accessing downloads.</i></p>
+                    </dd>
+                <dt><a href="https://discord.gg/HhMfr4N6H6">Nevercord - Neverball (Legacy)</a></dt>
+                    <dd><p>Play the classic game the way you know and love. Collect coins to activate the goal pad, and decide to collect them all before timer runs out.</p></dd>
+            </dl>
+        </div>
+
+<?php include("includes/footer.php"); ?>
